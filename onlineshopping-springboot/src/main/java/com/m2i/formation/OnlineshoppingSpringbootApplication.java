221bab22dfdb1cf3b5536239c50cf98ba1fb2f6c
walidhachani <walidhachani.5@gmail.com>
2018-07-03 00:41:16 +0200

    spring boot git configure

---
+++ onlineshopping-springboot/src/main/java/com/m2i/formation/OnlineshoppingSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.m2i.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshoppingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshoppingSpringbootApplication.class, args);
+	}
+}
